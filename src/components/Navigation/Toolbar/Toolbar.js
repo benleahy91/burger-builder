8a67b943b77a64cf7c8dc73f9af041338a9cde84
benleahy91 <benleahy91@gmail.com>
2019-02-12 15:55:13 -0500

    responsive adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,13 +7,11 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
 	<header className={ classes.Toolbar }>
-		<div>
-			MENU
+		<div>MENU</div>
 			<Logo />
-			<nav>
+			<nav className={ classes.DesktopOnly }>
 				<NavigationItems />
 			</nav>
-		</div>
 	</header>
 );
 
