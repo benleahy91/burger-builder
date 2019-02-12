5bcc05f0ea594ab1c3f1b425996405d4bac73403
benleahy91 <benleahy91@gmail.com>
2019-02-12 09:50:13 -0500

    added logo and toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './Toolbar.css'
+import Logo from '../../Logo/Logo'
+
+
+const toolbar = (props) => (
+	<header className={ classes.Toolbar }>
+		<div>
+			MENU
+			<Logo />
+			<nav>
+			</nav>
+		</div>
+	</header>
+);
+
+export default toolbar;
