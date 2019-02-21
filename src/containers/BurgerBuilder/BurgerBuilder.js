b85db83725d1f1b96e28838bba40a705b8239f6e
benleahy91 <benleahy91@gmail.com>
2019-02-21 13:08:45 -0500

    checkout page and checkout page routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,28 +88,29 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHandler = () => {
 		// console.log('Continue');	
-		this.setState({ loading: true });
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Ben Leahy',
-				address: {
-					street: '123 Benmerica Industries Way',
-					zipCode: '12345',
-					country: 'United States'
-				},
-				email: 'ben@benmericaindustries.com'
-			},
-			deliveryMethod: 'fastest'
-		};
-		axios.post('/orders.json', order)
-			.then(response => {				
-				this.setState({ loading: false, purchasing: false });
-			})
-			.catch(error => {
-				this.setState({ loading: false, purchasing: false });
-			})
+		// this.setState({ loading: true });
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Ben Leahy',
+		// 		address: {
+		// 			street: '123 Benmerica Industries Way',
+		// 			zipCode: '12345',
+		// 			country: 'United States'
+		// 		},
+		// 		email: 'ben@benmericaindustries.com'
+		// 	},
+		// 	deliveryMethod: 'fastest'
+		// };
+		// axios.post('/orders.json', order)
+		// 	.then(response => {				
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({ loading: false, purchasing: false });
+		// 	})
+		this.props.history.push('/checkout')
 	};
 
 	render() {
