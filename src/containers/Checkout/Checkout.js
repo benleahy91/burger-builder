5d0e4e14a80ed7542fb10d0fcd173ad3c29653fd
benleahy91 <benleahy91@gmail.com>
2019-03-13 11:14:05 -0400

    redux purchases and orders

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/index'
+import button from '../../components/UI/Button/Button';
 
 class Checkout extends Component {
 
@@ -15,27 +17,33 @@ class Checkout extends Component {
 		this.props.history.replace('/checkout/contact-data');
 	};
 
-	render() {		
-		return (
-			<div>
-				<CheckoutSummary
-					ingredients={ this.props.ings }
-					checkoutCancelled={ this.checkoutCancelledHandler }
-					checkoutContinued={ this.checkoutContinuedHandler } />
-				<Route 
-					path={this.props.match.path + '/contact-data'} 
-					component={ ContactData } />
-			</div>
-		);
+	render() {
+		let summary = <Redirect to="/" />	
+
+		if (this.props.ings) {
+			const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
+			summary = (
+				<div>
+					{ purchasedRedirect }
+					<CheckoutSummary
+						ingredients={ this.props.ings }
+						checkoutCancelled={ this.checkoutCancelledHandler }
+						checkoutContinued={ this.checkoutContinuedHandler } />	
+					<Route 
+						path={ this.props.match.path + '/contact-data' } 
+						component={ ContactData } />
+				</div>
+			);
+		};
+		return summary;
 	};
 };
 
 const mapStateToProps = state => {
 	return {
-		ings: state.ingredients,
+		ings: state.burgerBuilder.ingredients,
+		purchased: state.order.purchased
 	};
 };
 
 export default connect(mapStateToProps)(Checkout);
-
-// price={ this.state.totalPrice } 
