a2ff5db316631d5d1d5bd4f703650cf36c43ebfc
benleahy91 <benleahy91@gmail.com>
2019-04-03 18:05:08 -0400

    redux devtools test

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+console.log(process.env.NODE_ENV);
+
 
 const rootReducer = combineReducers ({
 	burgerBuilder: burgerBuilderReducer,
